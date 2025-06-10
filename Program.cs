05727c0b4968eb971221daaf6a333ba6a1644880
Mirka Monzón <35476538+MirkaNicolle@users.noreply.github.com>
2025-06-09 23:14:45 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+// Program.cs
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// SOLO UNA VEZ:
+builder.Services.AddDbContext<MiProyectoWeb.Data.AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
